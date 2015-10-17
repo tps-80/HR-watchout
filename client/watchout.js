35a199adc9237f3e3f6d9f30bf048f4962dce22b
tps-80 <thomaspsorensen@gmail.com>
2015-10-16 20:44:03 -0700

    Enemies move to random positions

--- client/watchout.js
+++ client/watchout.js
@@ -15,11 +15,10 @@ var enemyMaker = function(){
     }
   });
 }
-console.log(enemyMaker)
-enemies = enemyMaker();
-console.log(enemies)
 
-var enemyPop = 
+enemies = enemyMaker();
+
+var enemyPop =
   container.selectAll('circle').data(enemies, function(d) { return d.id })
   .enter()
   .append("circle")
@@ -27,22 +26,35 @@ var enemyPop =
   .attr("cy", function(d) { return d.y })
   .attr("r", 20);
 
+var enemyMove = function() {
+  enemies = enemyMaker();
+  container.selectAll('circle').data(enemies, function(d) { return d.id })
+  .transition()
+  .duration(1000)
+  // .enter()
+  // .append("circle")
+  .attr("cx", function(d) { return d.x })
+  .attr("cy", function(d) { return d.y })
+  // .attr("r", 20)
+ 
+}
+
   // var player = container.selectAll("rect")
   //   .append("rect")
   //   .attr("width", 50)
   //   .attr("height", 50)
   //   .attr("fill", "orange")
 
-  var move = function() {
-    for (var i = 0; i < enemies.length; i++) {
-      enemies[i].x = Math.random() * 700;
-      enemies[i].y = Math.random() * 500;
-      enemyPop.transition().attr("cx", enemies[i].x).attr("cy", enemies[i].y);
-    }
+  // var move = function() {
+  //   for (var i = 0; i < enemies.length; i++) {
+  //     enemies[i].x = Math.random() * 700;
+  //     enemies[i].y = Math.random() * 500;
+  //     console.log(enemies[i]);
+  //   }
     
-  }
+  // }
 
-  setInterval(move, 500);
+  setInterval(enemyMove, 1000);
 
 
 
